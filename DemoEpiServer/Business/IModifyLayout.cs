5f6cd2aab7890d2e6c4320897564e65e310c331e
asi318 <asi318@DELASI31843106>
2017-11-27 12:30:14 +0530

    Add project files.

---
+++ DemoEpiServer/Business/IModifyLayout.cs
@@ -0,0 +1,13 @@
+using DemoEpiServer.Models.ViewModels;
+
+namespace DemoEpiServer.Business
+{
+    /// <summary>
+    /// Defines a method which may be invoked by PageContextActionFilter allowing controllers
+    /// to modify common layout properties of the view model.
+    /// </summary>
+    interface IModifyLayout
+    {
+        void ModifyLayout(LayoutModel layoutModel);
+    }
+}
