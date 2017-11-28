fe77ec6e9f4451c8f8f92b0098403a9bbd4c2bf3
asi318 <asingh319@sapient.com>
2017-11-28 11:34:48 +0530

    changes in project file

---
+++ DemoEpiServer/Models/Blocks/AccordionBlock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace DemoEpiServer.Models.Blocks
+{
+    [ContentType(DisplayName = "AccordionBlock", GUID = "87c9955e-3f88-491d-9b71-41a8bbf6a586", Description = "")]
+    public class AccordionBlock : SiteBlockData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Name",
+            Description = "Name field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Name",
+            Description = "Name field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual string Name { get; set; }
+
+    }
+}
