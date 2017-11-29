eaada51e5e2cb63609c3e3e209c3d5d60e922e39
Mandeep Singh <34021647+msingh21@users.noreply.github.com>
2017-11-29 10:50:01 +0530

    Added template view
    Added common page template view

--- DemoEpiServer/Models/Pages/CommonPage.cs
+++ DemoEpiServer/Models/Pages/CommonPage.cs
@@ -9,7 +9,7 @@ namespace DemoEpiServer.Models.Pages
 {
     [SiteContentType(DisplayName = "Common Page", GUID = "d239e6d5-889b-4904-b650-da2b20867d49", GroupName = Global.GroupNames.Specialized, Description = "")]
     //[SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-standard.png")]
-    public class CommonPage : SitePageData, IContainerPage
+    public class CommonPage : SitePageData
     {
         [Display(
              GroupName = SystemTabNames.Content,
