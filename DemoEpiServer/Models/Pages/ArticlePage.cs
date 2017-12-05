34bd5e02d49feca070c0aaa4920b142c7779fbc1
Neha Agarwal <nagarwal@sapient.com>
2017-12-05 12:09:00 +0530

    included all types in start page

--- DemoEpiServer/Models/Pages/ArticlePage.cs
+++ DemoEpiServer/Models/Pages/ArticlePage.cs
@@ -12,7 +12,7 @@ namespace DemoEpiServer.Models.Pages
         GroupName = Global.GroupNames.News,
         GUID = "AEECADF2-3E89-4117-ADEB-F8D43565D2F4")]
     [SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-article.png")]
-    public class ArticlePage : StandardPage
+    public class ArticlePage : SitePageData
     {
         [CultureSpecific]
         [Display(
